037a62475944510d2c4c48d3589083273ad36e00
bogeychan <bogeychan@outlook.com>
2023-04-24 22:04:12 +0200

    fix: ignored context headers

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ import { Elysia } from 'elysia'
 
 export const html = () => (app: Elysia) =>
     app
-        .decorate(
-            'html',
-            (value: string) =>
-                new Response(value, {
+        .derive((context) => ({
+            html(value: string) {
+
+                //! error: expect(received).toBe(expected)
+                //! Expected: "text/html"
+                //! Received: "text/html, text/html"
+
+                // context.set.headers['content-type'] = 'text/html';
+                // return new Response(value, { headers: context.set.headers });
+
+                return new Response(value, {
                     headers: {
+                        ...context.set.headers,
                         'content-type': 'text/html'
                     }
                 })
-        )
+            }
+        }))
         .onAfterHandle((context, response) => {
             if (
                 typeof response === 'string' &&
