497d6a4f6e652ddc6fac7899e9fbc37284587182
Arthur Fiorette <me@arthur.place>
2023-10-07 16:36:38 -0300

    global var

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,3 @@ export * from './utils'
 // Adds @kitajs/html's Html globally.
 export * from '@kitajs/html/register'
 export { Html } from '@kitajs/html'
-
-declare global {
-	/** The html factory namespace. */
-	var Html: typeof import('./index')
-}
