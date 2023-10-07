629c60312d5e97cbd34e5de9ce3f25123a1a6e6b
saltyaom <saltyaom@gmail.com>
2023-10-08 02:34:07 +0700

    :tada: feat: auto register html

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,8 @@ export * from './options'
 export * from './utils'
 
 export { Html } from '@kitajs/html'
+
+declare global {
+	/** The html factory namespace. */
+	var Html: typeof import('./index')
+}
