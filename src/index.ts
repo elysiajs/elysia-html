1704a2c6d219b297e7c454ba35d6eaa526b6f15c
Arthur Fiorette <me@arthur.place>
2023-10-07 16:28:46 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-// Adds @kitajs/html's Html globally.
-// @ts-expect-error - When running from a compiled bun version,
-// this will fail because /register will be stripped out if not
-// used. This is just a workaround to make it work.
-import { noop } from '@kitajs/html/register'
-try {
-	noop()
-} catch {}
-
 export * from './html'
 export * from './options'
 export * from './utils'
 
+// Adds @kitajs/html's Html globally.
+export * from '@kitajs/html/register'
 export { Html } from '@kitajs/html'
