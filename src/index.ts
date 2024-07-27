4cf7c37e3806ae786982d6163aa2eac829b6d31b
x4132 <31595285+x4132@users.noreply.github.com>
2024-07-27 14:29:40 -0700

    Restructure/Create Html Object from destructured @kitajs/html exports

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,24 @@ export * from './utils'
 
 export { ErrorBoundary } from '@kitajs/html/error-boundary'
 
-export { Html } from '@kitajs/html'
+import {escape, escapeHtml, isVoidElement, attributesToString, toKebabCase, isUpper, styleToString, h, contentsToString, contentToString, Fragment} from "@kitajs/html";
+
+export const Html = {
+  escape,
+  e: escape,
+  escapeHtml,
+  isVoidElement,
+  attributesToString,
+  toKebabCase,
+  isUpper,
+  styleToString,
+  createElement: h,
+  h,
+  contentsToString,
+  contentToString,
+  Fragment
+};
+
 export { createElement }
 
 export { html as default } from './html'
